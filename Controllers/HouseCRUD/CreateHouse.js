f65eca9fd443eae432be787d32eadb3540ccfa0c
Abdelrahman Alasal <AbdelrahmanAlasal@gmail.com>
2022-09-15 21:07:16 +0300

    done

--- Controllers/HouseCRUD/CreateHouse.js
+++ Controllers/HouseCRUD/CreateHouse.js
@@ -1,29 +1,41 @@
 const houseModel = require("../../Models/House");
-let handelCreate= async(req,res)=>{
-    let{imgURL,ownerName,houseAddress,email,houseSize,numOfRoomse,isSold,price,phoneNumber,ispremium,status}=req.body;
-    await houseModel.create({
-        imgURL: imgURL,         
-        ownerName:ownerName,
-        houseAddress :houseAddress,
-        houseSize :houseSize,
-        numOfRoomse:numOfRoomse,
-        isSold:isSold,
-        price:price,
-        email:email,
-        phoneNumber:phoneNumber,
-        ispremium:ispremium,
-        status:status
-    })
-    houseModel.find({email:email},(err,result)=>{
-      if(err)
-      {
-          console.log(err);
-      }
-      else
-      {
-          
-          res.send(result);
-      }
-  })
-  } 
-  module.exports=handelCreate
+let handelCreate = async (req, res) => {
+  let {
+    imgURL,
+    ownerName,
+    houseAddress,
+    email,
+    houseSize,
+    numOfRoomse,
+    isSold,
+    price,
+    phoneNumber,
+    ispremium,
+    status,
+    lat,
+    long,
+  } = req.body;
+  await houseModel.create({
+    imgURL: imgURL,
+    ownerName: ownerName,
+    houseAddress: houseAddress,
+    houseSize: houseSize,
+    numOfRoomse: numOfRoomse,
+    isSold: isSold,
+    price: price,
+    email: email,
+    phoneNumber: phoneNumber,
+    ispremium: ispremium,
+    status: status,
+    lat: lat,
+    long: long,
+  });
+  houseModel.find({ email: email }, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+};
+module.exports = handelCreate;
--- Controllers/HouseCRUD/UpdateUserHouse.js
+++ Controllers/HouseCRUD/UpdateUserHouse.js
@@ -1,29 +1,66 @@
 const houseModel = require("../../Models/House");
-let handelUserUpdate=(req,res)=>{
-    let id=req.params.id
-    let email=req.params.email
-    let{imgURL,ownerName,houseAddress,houseSize,numOfRoomse,isSold,price,phoneNumber,status}=req.body;
-    console.log(req.body);
-    houseModel.findByIdAndUpdate(id,{imgURL,ownerName,houseAddress,email,houseSize,numOfRoomse,isSold,price,phoneNumber,status},(err,result)=>{
-  
-        if(err) {
+let handelUserUpdate = (req, res) => {
+  let id = req.params.id;
+  let email = req.params.email;
+  let {
+    imgURL,
+    ownerName,
+    houseAddress,
+    houseSize,
+    numOfRoomse,
+    isSold,
+    price,
+    phoneNumber,
+    status,
+  } = req.body;
+  console.log(req.body);
+  houseModel.findByIdAndUpdate(
+    id,
+    {
+      imgURL,
+      ownerName,
+      houseAddress,
+      email,
+      houseSize,
+      numOfRoomse,
+      isSold,
+      price,
+      phoneNumber,
+      status,
+    },
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        houseModel.find({ email: email }, (err, result) => {
+          if (err) {
             console.log(err);
-        }
-        else {
-            houseModel.find({email:email},(err,result)=>{
-                if(err)
-                {
-                    console.log(err);
-                }
-                else
-                {
-                  console.log(result)
-                    res.send(result);
-                    
-                }
-            })
-        }
-    })
+          } else {
+            console.log(result);
+            res.send(result);
+          }
+        });
+      }
+    }
+  );
+};
+module.exports = handelUserUpdate;
+
+/*
+  let Data = [];
+
+  let filterd = data.filter{i =>{
+    if (i.address.includes(search))
+   else if (i.name.includes(search))
+    else if  (i.name.includes(search))
+    else if  (i.name.includes(search))
+    else if (i.name.includes(search))
+    else
+    return false
+
+  }}
+
   
-  }
-  module.exports= handelUserUpdate
+
+
+  */
